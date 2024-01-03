78cc24a14355fad2aadc8907085b6411f266fdae
Tameem Ansari <tameemansarim11@gmail.com>
2024-01-03 17:48:12 +0200

    paybill

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7a1665fa7d80f930ddb7272f245d509c6dc5d712',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7a1665fa7d80f930ddb7272f245d509c6dc5d712',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v13.7.0',
+            'version' => '13.7.0.0',
+            'reference' => '1f60eb1a1b86b29f2ba0e14c0ae905ca838c1cdd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
